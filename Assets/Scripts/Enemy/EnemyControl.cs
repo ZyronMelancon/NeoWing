f4ebbb07c30fd1f85b81b0aad3dc34d8f8b8a97c
ZyronMelancon <altausolaris@gmail.com>
2017-05-09 06:17:47 -0500

    Build 050917-1
    Moved everything over from the original "ItCostsAnArmAndA" repo. I am
    continuing to build this, with the intention of making a full game out
    of it.
    
    Changes since last version:
    +Normalized vectors of enemy bullets
    -Turned off music for now
    +Added better sound effects! Much more satisfying, less annoying
    +Made small enemy ships move around while shooting
    +Made small enemy ships turn red as their health goes down
    +Made health text change color as it goes below 20
    -Taking out starting scene
    -Hid help screen at the beginning
    
    Plans:
    >Make spinning animation for small enemy ships
    >Make menu screen
    >Make sound for getting hit
    >Add "bomb" ability that wipes out bullets on screen
    >Give 20 damage to player ship if they run into enemy
    
    Ideas:
    >Point system to buy upgrades
    >Unlockables
    >Music selection
    >Music On/Off
    >Story mode
    >Wider array of enemies
    >>Spiral pattern enemy
    >>Spread pattern enemy
    >> Kamikaze enemy
    >>Missile enemy
    >Temporary upgrades
    >>Speed boost
    >>Firing rate boost
    >>Forcefield/Reflector
    >>Longer grabber arm

---
+++ Assets/Scripts/Enemy/EnemyControl.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyControl : MonoBehaviour {
+
+    public GameObject m_Shot;
+    public ParticleSystem m_Explosion;
+    public float m_ShootSpeed;
+    public float m_StartPos;
+    public float m_ZoomInPos;
+    public float m_MoveSpeed;
+    public float m_Health;
+    public float m_MoveTime;
+
+    AudioSource m_ExplSFX;
+    HUDUpdate HUD;
+    EnemyManager EnMan;
+    ShipControl ship;
+    Rigidbody2D m_Body;
+    public float m_WantedPos;
+
+    void Start ()
+    {
+        m_Body = GetComponent<Rigidbody2D>();
+        ship = GameObject.Find("Ship").GetComponent<ShipControl>();
+        HUD = GameObject.Find("HUD").GetComponent<Canvas>().GetComponent<HUDUpdate>();
+        StartCoroutine(Shoot());
+        StartCoroutine(NewPos());
+        EnMan = GameObject.Find("HUD").GetComponent<EnemyManager>();
+        m_ExplSFX = GameObject.Find("EnemyKills").GetComponent<AudioSource>();
+	}
+
+    void Update()
+    {
+        if (m_Health <= 0)
+        {
+            HUD.enemykills++;
+            EnMan.m_EnemiesAlive--;
+            Destroy(gameObject);
+        }
+        Move();
+        UpdateColor();
+    }
+
+    IEnumerator Shoot()
+    {
+        while(!ship.gameover)
+        {
+            yield return new WaitForSeconds(m_ShootSpeed);
+            if (!ship.gameover)
+            {
+                Vector3 shotpos = transform.position + new Vector3(0, 0, -0.5f);
+                Instantiate(m_Shot, shotpos, transform.rotation);
+            }
+        }
+    }
+
+    IEnumerator NewPos()
+    {
+        while (!ship.gameover)
+        {
+            yield return new WaitForSeconds(m_MoveSpeed + Random.Range(-0.5f,1.5f));
+            if (!ship.gameover)
+            {
+                m_WantedPos = Random.Range(-3, 3);
+                m_ZoomInPos = Random.Range(2.4f, 5.5f);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "PlayerShot")
+        {
+            Destroy(other.gameObject);
+            m_Health -= 5;
+            HUD.hits++;
+        }
+    }
+
+    void UpdateColor()
+    {
+        GetComponent<SpriteRenderer>().color = new Color32(255, System.Convert.ToByte(150 + (105 * m_Health / 20)), System.Convert.ToByte(150 + (105 * m_Health / 20)), 255);
+    }
+
+    void OnDestroy()
+    {
+        m_Explosion.transform.parent = null;
+        m_Explosion.Play();
+        m_ExplSFX.Play();
+        Destroy(m_Explosion.gameObject, m_Explosion.duration + 1f);
+    }
+
+    void Move()
+    {
+        Vector3 moveVel = m_Body.velocity;
+
+        moveVel.y = 0;
+        moveVel.x = 0;
+
+        if (!ship.gameover)
+        {
+            moveVel.y = moveVel.y + (m_ZoomInPos - transform.position.y);
+            moveVel.x = moveVel.x + (m_WantedPos - transform.position.x);
+        }
+        else
+            moveVel.y = moveVel.y + (11 - transform.position.y);
+
+        m_Body.velocity = moveVel;
+    }
+}
---
+++ Assets/Scripts/Ship/ShipControl.cs
@@ -0,0 +1,229 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShipControl : MonoBehaviour {
+
+    public float m_BaseSpeed = 1.0f;
+    public float m_BoostMultiplier = 1.5f;
+    public float m_SlowMultiplier = 0.5f;
+    public float m_TurnTilt = 100f;
+    public float m_TiltSpeed = 1f;
+    public GameObject m_ShotSmall;
+    public Transform m_ShootPos;
+    public Slider m_ArmJoint;
+    public GameObject m_Grabber;
+
+    public int m_ShotsFired = 0;
+    public int m_Fuel = 100;
+    public int m_Health = 100;
+    public bool gameover = false;
+
+    Rigidbody2D m_Body;
+    Transform m_ShipPos;
+    public ParticleSystem m_Explosion;
+    public AudioSource m_ExplosionSFX;
+
+    private bool shooting = false;
+    private bool boosting = false;
+    private bool timething = false;
+
+    private float roboarmlength = 0;
+    private bool callOnce = false;
+
+	// Use this for initialization
+	void Start () {
+        m_Body = GetComponent<Rigidbody2D>();
+        m_ShipPos = GetComponent<Transform>();
+        StartCoroutine(fuelLoss());
+        StartCoroutine(roboarm());
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        StartCoroutine(Shoot());
+
+        grabber();
+        
+        shipRotate(Input.GetAxisRaw("Horizontal"));
+
+        
+
+        if (m_Health <= 0)
+        {
+            m_Health = 0;
+            gameover = true;
+            Destroy(gameObject);
+        }
+
+        if(m_Fuel <= 0)
+        {
+            m_Fuel = 0;
+            gameover = true;
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        if (Input.GetAxisRaw("Fire1") == 1 && shooting == false)
+        {
+            shooting = true;
+            Instantiate(m_ShotSmall, m_ShootPos.position, m_ShootPos.rotation);
+            m_ShotsFired++;
+            yield return new WaitForSeconds(0.1f);
+            shooting = false;
+        }
+    }
+
+    IEnumerator fuelLoss()
+    {
+        while (!gameover)
+        {
+            if (m_Fuel >= 0 && Input.GetAxisRaw("Boost") == 1 && !boosting)
+            {
+
+                boosting = true;
+                yield return new WaitForSeconds(1f);
+                m_Fuel--;
+                boosting = false;
+
+            }
+            else if (m_Fuel >= 0 && !boosting && !timething)
+            {
+                timething = true;
+                yield return new WaitForSeconds(2.5f);
+                m_Fuel--;
+                timething = false;
+            }
+        }
+    }
+
+    IEnumerator roboarm()
+    {
+            if (Input.GetAxisRaw("Fire2") == 1 && !callOnce)
+            {
+                callOnce = true;
+                while (roboarmlength < 30)
+                {
+                    yield return new WaitForSeconds(0.02f);
+                    roboarmlength += 1;
+                    m_ArmJoint.value = roboarmlength;
+                }
+                while (roboarmlength > 0)
+                {
+                    yield return new WaitForSeconds(0.02f);
+                    roboarmlength -= 1;
+                    m_ArmJoint.value = roboarmlength;
+                }
+            }
+            callOnce = false;
+    }
+
+    void grabber()
+    {
+        if(Input.GetAxisRaw("Fire2") == 1)
+        {
+            m_Grabber.SetActive(true);
+            if (m_Grabber.transform.localPosition.y < 1)
+                roboarmlength = 1;
+            else
+                roboarmlength = 0;
+            m_Grabber.transform.Translate(0,roboarmlength/5,0);
+        }
+        else
+        {
+            if (m_Grabber.transform.localPosition.y > 0)
+                roboarmlength = 1;
+            else
+                roboarmlength = 0;
+            m_Grabber.transform.Translate(0, -roboarmlength/5, 0);
+            if (roboarmlength == 0)
+                m_Grabber.SetActive(false);
+        }
+    }
+
+    public void Move(float hinput, float vinput)
+    {
+        Vector3 moveVel = m_Body.velocity;
+        float currentspeed = 0;
+
+        if(roboarmlength > 0)
+        {
+            hinput = 0;
+        }
+
+        if (!gameover)
+        {
+            if (Input.GetAxisRaw("Boost") == 1)
+            {
+                currentspeed = m_BaseSpeed * m_BoostMultiplier;
+                moveVel.x = hinput * currentspeed;
+                moveVel.y = vinput * currentspeed;
+            }
+            else if (Input.GetAxisRaw("Slow") == 1)
+            {
+                currentspeed = m_BaseSpeed * m_SlowMultiplier;
+                moveVel.x = hinput * currentspeed;
+                moveVel.y = vinput * currentspeed;
+            }
+            else
+            {
+                currentspeed = m_BaseSpeed;
+                moveVel.x = hinput * currentspeed;
+                moveVel.y = vinput * currentspeed;
+            }
+        }
+
+        if(vinput == 0 && Input.GetAxisRaw("Boost") == 0)
+        {
+            moveVel.y = 0;
+            moveVel.y = moveVel.y + (-5.8f - m_ShipPos.position.y);
+        }
+        else if(gameover)
+        {
+            moveVel.y = 0;
+            moveVel.x = 0;
+            moveVel.y = moveVel.y + (-5.8f - m_ShipPos.position.y);
+        }
+
+        if (m_ShipPos.position.y >= 5.8f)
+            moveVel.y = moveVel.y - currentspeed;
+        if (m_ShipPos.position.y <= -5.8f)
+            moveVel.y = moveVel.y + currentspeed;
+        if (m_ShipPos.position.x >= 3f)
+            moveVel.x = moveVel.x - currentspeed;
+        if (m_ShipPos.position.x <= -3f)
+            moveVel.x = moveVel.x + currentspeed;
+
+        m_Body.velocity = moveVel;
+
+    }
+
+    public void shipRotate(float hinput)
+    {
+        float rotationSum = hinput * -m_TurnTilt;
+        //float slowrot = 
+        m_Body.MoveRotation(rotationSum);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "EnemyShot")
+        {
+            EnemyShotControl enshot = other.gameObject.GetComponent<EnemyShotControl>();
+            m_Health -= enshot.m_ShotDamage;
+            Destroy(other.gameObject);
+        }
+    }
+
+    void OnDestroy()
+    {
+        m_Explosion.transform.parent = null;
+        m_Explosion.Play();
+        m_ExplosionSFX.Play();
+        Destroy(m_Explosion.gameObject, m_Explosion.duration + 3);
+    }
+
+}
